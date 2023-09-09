0af0905a320fbb2427a65b3e17d9d84b75f523a2
nan mu <mu.nan.11@outlook.com>
2023-09-09 18:05:56 +0800

    做完了第一天的第一题，耗时这么久主要是调试环境 现在的解决方案是wsl+clang+clangd+lldb+mingw 评价是一碰上pthean就寄

--- include/linkList.h
+++ include/linkList.h
@@ -1,8 +1,9 @@
 #ifndef NODELIST_H
 #define NODELIST_H
-#define CLEARD 0
+#define CLEAN 0
 
 #include <cstdio>
+#include <cstdlib>
 
 typedef struct Node {  // 数据节点类
 	Node *next;
@@ -17,7 +18,7 @@ class NodeList {
 
   public:
 	NodeList() {
-		printf("A node list was created!");
+		// printf("A node list was created!");
 		head = nullptr;
 		tail = nullptr;
 		opt = nullptr;
@@ -27,5 +28,61 @@ class NodeList {
 	bool sort();
 	bool pop();	 //单链表的话，就当队列用吧，当堆的话pop的实现就太傻了
 };
+
+inline bool NodeList::push(int data) {
+	opt = (Node *)malloc(sizeof(Node));
+	opt->data = data;
+	opt->next = nullptr;
+	if (tail == nullptr) {	//创建首个节点的情况
+		// printf("创建了第一个节点");
+		head = opt;
+		tail = opt;
+		opt = nullptr;
+	} else {  //创建后续节点的情况
+		// printf("创建了一个节点");
+		tail->next = opt;
+		tail = opt;
+		opt = nullptr;
+	}
+	return true;
+}
+
+inline bool NodeList::pop() {
+	if (head == tail) {
+		free(head);
+		head = nullptr;
+		tail = nullptr;
+		printf("clean");
+		return CLEAN;
+	} else {
+		opt = head;
+		head = opt->next;
+		free(opt);
+		opt = nullptr;
+		return true;
+	}
+}
+
+inline bool NodeList::sort() {
+	Node *opt2 = nullptr;
+	for (opt = head; opt != nullptr; opt = opt->next) {
+		for (opt2 = opt; opt2 != nullptr; opt2 = opt2->next) {	//冒泡排序
+			if (opt2->data < opt->data) {
+				opt->data += opt2->data;
+				opt2->data = opt->data - opt2->data;
+				opt->data = opt->data - opt2->data;
+			}
+		}
+	}
+	return true;
+}
+
+inline bool NodeList::print() {
+	for (opt = head; opt != nullptr; opt = opt->next) {
+		printf("%d ", opt->data);
+	}
+	printf("\n");
+	return true;
+}
 
 #endif
--- src/day1.cpp
+++ src/day1.cpp
@@ -6,7 +6,27 @@ using namespace std;
 
 #define PROJECT_NAME "DAY1"
 
+#include "comFunc.hpp"	//一些常用的函数
+
 int main() {
-  printf("bfnbfbf\n");
-  NodeList line;
+	vector<int> rand_data;	// 准备一些随机数
+
+	com_func::randomInitArr(rand_data, 100);
+
+	//  往链表里放入数据
+	NodeList line;
+	for (vector<int>::iterator iter = rand_data.begin();
+		 iter != rand_data.end(); ++iter)
+		line.push(*iter);
+
+	//排序
+	line.sort();
+
+	// 输出
+	line.print();
+
+	// 删除
+	while (line.pop()) {
+	}
+	return 0;
 }
