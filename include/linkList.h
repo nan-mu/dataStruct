69bf961318bdcb44bcb41d5d3034d38e9028c660
nan mu <mu.nan.11@outlook.com>
2023-09-09 11:04:30 +0800

    我的妈，终于完成，新建项目

---
+++ include/linkList.h
@@ -0,0 +1,9 @@
+#ifndef MYCLASS_H
+#define MYCLASS_H
+
+typedef struct Node {  // 数据节点类
+    Node *next;
+    int data;
+}Node;
+
+#endif
---
+++ src/day1.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include "linkList.h"
+
+using namespace std;
+
+#define PROJECT_NAME "DAY1"
+
+int main() {
+    printf("f\n");
+    Node a;
+    a.data = 1;
+}
